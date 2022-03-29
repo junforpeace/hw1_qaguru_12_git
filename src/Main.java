feba2fb881137b140e017d0813452bb3349456f5
junforpeace <soapcream@gmail.com>
2022-03-29 22:43:33 +0200

    добавлены переменные

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 11;
+        int b = 6;
+        int c = minFunction(a, b);
+
         System.out.println("Hello, sun!");
         System.out.println("Hello, sky!");
-        System.out.println("Hello, BODY!");
+        System.out.println("Hello, 123456BODY!");
+
+    }
+
+    public static int minFunction(int a, int b) {
+        int c;
+        if (a > b)
+            c = b;
+        else
+            c = a;
+
+        return c;
 
     }
 }
