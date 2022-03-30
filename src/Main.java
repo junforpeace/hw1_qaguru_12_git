c6b2c446b8b2cde902fffdf32fabe0df976e7f15
junforpeace <soapcream@gmail.com>
2022-03-30 10:42:29 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 11;
-        int b = 6;
-        int minFunction = minFunction(a, b);
+
 
         System.out.println("Hello, sun!");
         System.out.println("Hello, sky!");
@@ -11,15 +9,6 @@ public class Main {
 
     }
 
-    public static int minFunction(int a, int b) {
-        int c;
-        if (a > b)
-            c = b;
-        else
-            c = a;
 
-        return c;
-
-    }
 }
 
